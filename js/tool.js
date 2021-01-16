24c756b72a3c97d748fb5ab1079f22581798aa22
tenma <1014203967@qq.com>
2021-01-16 19:03:55 +0800

    feat: 生成页面布局调试

--- js/tool.js
+++ js/tool.js
@@ -1,10 +1,10 @@
 var jsonData;
 var resultData;
-$(document).ready(function(){
+$(document).ready(function () {
 
 
-     // 初始化数据
-     initData();
+    // 初始化数据
+    initData();
 
 
 
@@ -12,18 +12,18 @@ $(document).ready(function(){
 
 
     // 读取data.js
-    function initData(){
+    function initData() {
         $.ajax({
             url: "data/data.json",
             type: "GET",
             dataType: "json",
-            success: function(data) {
+            success: function (data) {
                 console.log(data)
                 jsonData = data;
                 //调用布局方法
                 layout();
             },
-            error: function(data){
+            error: function (data) {
                 console.log(data)
             }
         })
@@ -36,11 +36,11 @@ $(document).ready(function(){
     // "events":[] -->
 
 
-   
 
-    function layout(){
+
+    function layout() {
         var types = jsonData['types']
-        $.each(types, function(p1, p2) {
+        $.each(types, function (p1, p2) {
 
             //添加标题
             var typeUl = $('<h1 class="event-type">');
@@ -78,17 +78,25 @@ $(document).ready(function(){
 
             $('body').append(typeUl)
             $('body').append(typeDiv)
-            $('body').append(eventLabel)
+            // $('body').append(eventLabel)
+            addLine();
             // 活动数组
             var events = this['events'];
-            $.each(events, function(p1, p2) {
+            $.each(events, function (p1, p2) {
                 // 活动数据
-               creatEvent(this)
+                creatEvent(this)
+                addLine();
             })
-            
+
         });
 
-        
+
+    }
+
+
+    function addLine() {
+        var lineDiv = $('<div class="line-div"></div>')
+        $('body').append(lineDiv);
     }
 
 
@@ -104,6 +112,15 @@ $(document).ready(function(){
     // "cards"-->
 
     function creatEvent(event) {
+
+        var eventPevent = $('<div id="'+event['name']+'">')
+
+
+
+        var eventTitle = $('<h3>' + event['name'] + '</h3>')
+
+        eventPevent.append(eventTitle);
+
         var eventDiv = $('<div class="event-name-div">')
 
         // ==============名称=============
@@ -125,67 +142,67 @@ $(document).ready(function(){
 
         // ==============开始时间=============
         var startDateDiv = $('<div class="event-startdate-div">')
-         //开始日期
-         var startDateLabel = $('<label>');
-         startDateLabel.text('开始日期:')
-         var startDateInput = $('<input class="start-date">');
-         startDateInput.val(event['startDate'])
-         startDateDiv.append(startDateLabel)
-         startDateDiv.append(startDateInput)
- 
-         //开始时间
-         var startTimeLabel = $('<label>');
-         startTimeLabel.text('开始时间:')
-         var startTimeInput = $('<input class="start-time">');
-         startTimeInput.val(event['startTime'])
-         startDateDiv.append(startTimeLabel)
-         startDateDiv.append(startTimeInput)
+        //开始日期
+        var startDateLabel = $('<label>');
+        startDateLabel.text('开始日期:')
+        var startDateInput = $('<input class="start-date">');
+        startDateInput.val(event['startDate'])
+        startDateDiv.append(startDateLabel)
+        startDateDiv.append(startDateInput)
+
+        //开始时间
+        var startTimeLabel = $('<label>');
+        startTimeLabel.text('开始时间:')
+        var startTimeInput = $('<input class="start-time">');
+        startTimeInput.val(event['startTime'])
+        startDateDiv.append(startTimeLabel)
+        startDateDiv.append(startTimeInput)
 
         // ==============结束时间=============
-         var endDateDiv = $('<div class="event-enddate-div">')
-         //开始日期
-         var endDateLabel = $('<label>');
-         endDateLabel.text('结束日期:')
-         var endDateInput = $('<input class="end-date">');
-         endDateInput.val(event['endDate'])
-         endDateDiv.append(endDateLabel)
-         endDateDiv.append(endDateInput)
- 
-         //开始时间
-         var endTimeLabel = $('<label>');
-         endTimeLabel.text('结束时间:')
-         var endTimeInput = $('<input class="end-time">');
-         endTimeInput.val(event['endTime'])
-         endDateDiv.append(endTimeLabel)
-         endDateDiv.append(endTimeInput)
+        var endDateDiv = $('<div class="event-enddate-div">')
+        //开始日期
+        var endDateLabel = $('<label>');
+        endDateLabel.text('结束日期:')
+        var endDateInput = $('<input class="end-date">');
+        endDateInput.val(event['endDate'])
+        endDateDiv.append(endDateLabel)
+        endDateDiv.append(endDateInput)
+
+        //开始时间
+        var endTimeLabel = $('<label>');
+        endTimeLabel.text('结束时间:')
+        var endTimeInput = $('<input class="end-time">');
+        endTimeInput.val(event['endTime'])
+        endDateDiv.append(endTimeLabel)
+        endDateDiv.append(endTimeInput)
 
         // ==============关卡数=============
         var missionDiv = $('<div class="mission-div">')
-         //关卡数
-         var missionLabel = $('<label>');
-         missionLabel.text('关卡数:')
-         var missionInput = $('<input class="mission">');
-         missionInput.val(event['mission'])
-         missionDiv.append(missionLabel)
-         missionDiv.append(missionInput)
+        //关卡数
+        var missionLabel = $('<label>');
+        missionLabel.text('关卡数:')
+        var missionInput = $('<input class="mission">');
+        missionInput.val(event['mission'])
+        missionDiv.append(missionLabel)
+        missionDiv.append(missionInput)
 
-        $('body').append(eventDiv)
-        $('body').append(startDateDiv)
-        $('body').append(endDateDiv)
-        $('body').append(missionDiv)
+        eventPevent.append(eventDiv)
+        eventPevent.append(startDateDiv)
+        eventPevent.append(endDateDiv)
+        eventPevent.append(missionDiv)
 
         var cards = event['cards'];
         var table = $("<table border=\"1\">");
-        $('body').append(table)
-        var th = ["类型","中文(英文名)","日文名","图片名","档线"]
-        var keys = ["type","name","jpName","image","line"]
+        eventPevent.append(table)
+        var th = ["类型", "中文(英文名)", "日文名", "图片名", "档线"]
+        var keys = ["type", "name", "jpName", "image", "line"]
         var trHeader = $("<tr></tr>");
         trHeader.appendTo(table);
         for (var j = 0; j < th.length; j++) {
             var td = $("<td>" + th[j] + "</td>");
             td.appendTo(trHeader);
         }
-        $.each(cards, function(p1, p2) {
+        $.each(cards, function (p1, p2) {
             // 卡牌数据(用表格来存储)
             var tr = $("<tr></tr>");
             tr.appendTo(table);
@@ -194,11 +211,12 @@ $(document).ready(function(){
                 var td = $("<td>" + val + "</td>");
                 td.appendTo(tr);
             }
-            
+
         })
+        $('body').append(eventPevent);
     }
 
-     // <!-- 卡牌数组 -->
+    // <!-- 卡牌数组 -->
     // <!-- {
     //     "type": "UR(Demon)",
     //     "name":"Not Taking It Off",
@@ -209,13 +227,13 @@ $(document).ready(function(){
 
     // 收缩逻辑
 
-    function download(){
+    function download() {
         //TODO 获取数据的逻辑
         var blob = new Blob([JSON.stringify(jsonData)]);
         //对于Blob对象，我们可以创建出一个URL来访问它。使用URL对象的createObjectURL方法。
         var a = document.createElement('a');
         a.download = 'data.json';
-        a.href=window.URL.createObjectURL(blob);
+        a.href = window.URL.createObjectURL(blob);
         a.click()
     }
 });
